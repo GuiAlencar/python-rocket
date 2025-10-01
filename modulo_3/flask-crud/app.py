f0dc3f6f1e261d4cdbc5ce8d2370a0eaa0c16c7e
Guilherme <uchihaguilherme4@gmail.com>
2025-09-30 22:26:01 -0300

    adiciona conteudo

--- modulo_3/flask-crud/app.py
+++ modulo_3/flask-crud/app.py
@@ -16,5 +16,64 @@ def create_task():
     print(tasks)
     return jsonify({"message": "Nova tarefa criada com sucesso"})
 
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+    task_list = [task.to_dict() for task in tasks]
+    # outro jeito de fazer
+    # for task in tasks:
+    #     task_list.append(task.to_dict())
+    output = {
+        "tasks": task_list,
+        "total": len(task_list)
+    }
+    return jsonify(output)
+
+@app.route('/tasks/<int:id>', methods=['GET'])
+def get_task(id):
+    task = None
+    for t in tasks:
+        if t.id == id:
+            return jsonify(t.to_dict())
+    return jsonify({"message": "Tarefa não encontrada"}), 404
+
+# @app.route('/user/<int:user_id>')
+# def show_user(user_id):
+#     print(user_id)
+#     print(type(user_id))
+#     return f'User {user_id}'
+
+@app.route('/tasks/<int:id>', methods=['PUT'])
+def update_task(id):
+    task = None
+    for t in tasks:
+        if t.id == id:
+            task = t
+            break
+        
+    print(task)
+    if task == None:
+        return jsonify({"message": "Tarefa não encontrada"}), 404
+    
+    data = request.get_json()
+    task.title = data['title']
+    task.description = data['description']
+    task.completed = data['completed']
+    print(task)
+    return jsonify({"message": "Tarefa atualizada com sucesso"})
+
+@app.route('/tasks/<int:id>', methods=['DELETE'])
+def delete_task(id):
+    task = None
+    for t in tasks:
+        if t.id == id:
+            task = t
+            break
+
+    if task == None:
+        return jsonify({"message": "Tarefa não encontrada"}), 404
+    
+    tasks.remove(task)
+    return jsonify({"message": "Tarefa removida com sucesso"})
+
 if __name__ == '__main__':
     app.run(debug=True)
